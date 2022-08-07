7aff284b93565a2199d250fe0b23f0d323600dc4
mip33@yandex.ru <mip33@yandex.ru>
2022-08-07 10:54:56 +0300

    the end

---
+++ main.py
@@ -0,0 +1,53 @@
+class FlatIteratorEnhanced:
+
+    def __init__(self, multi_list):
+        self.multi_list = multi_list
+
+    def __iter__(self):
+        self.iterators_queue = []
+        self.current_iterator = iter(self.multi_list)
+        return self
+
+    def __next__(self):
+        while True:
+            try:
+                self.current_element = next(self.current_iterator)
+            except StopIteration:
+                if not self.iterators_queue:
+                    raise StopIteration
+                else:
+                    self.current_iterator = self.iterators_queue.pop()
+                    continue
+            if isinstance(self.current_element, list):
+                self.iterators_queue.append(self.current_iterator)
+                self.current_iterator = iter(self.current_element)
+            else:
+                return self.current_element
+
+def flat_generator_enhanced(multi_list):
+    for elem in multi_list:
+        if isinstance(elem, list):
+            for sub_elem in flat_generator_enhanced(elem):
+                yield sub_elem
+        else:
+            yield elem
+
+if __name__ == '__main__':
+
+    nested_list = [
+        ['a', ['b'], 'c'],
+        ['d', 'e', [[[[['f']]]]], 'h', False],
+        [1, [[[2]]], None],
+    ]
+
+    print('Вызов расширенного итератора:')
+    for item in FlatIteratorEnhanced(nested_list):
+        print(item)
+
+    print('Вызов компрехеншен:')
+    flat_list = [item for item in FlatIteratorEnhanced(nested_list)]
+    print(flat_list)
+
+    print('Вызов расширенного генератора:')
+    for item in flat_generator_enhanced(nested_list):
+        print(item)
